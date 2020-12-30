9ab2e18458d3e777567cf7ca08f3440f0db38e9a
Josh <59100881+JoshAnaba@users.noreply.github.com>
2020-12-30 15:23:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// $(document).ready(()=>{
+//   $("#my-photo").mouseover(()=>{
+//     $('#my-photo').css({
+//        left: '20px'
+//     })
+//   })
+
+//   $("#my-photo").mouseout(()=>{
+//     $('#my-photo').css({
+//         left:'-20px'
+//     })
+//   })
+// })
